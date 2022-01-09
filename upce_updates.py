f900c4a43872865921f4cc0689db3e0642c92512
Pavel Shishkin <41304680+ShishkinPavel@users.noreply.github.com>
2022-01-09 01:19:26 +0100

    Update upce_updates.py

--- upce_updates.py
+++ upce_updates.py
@@ -34,10 +34,10 @@ def send_updates():
             for i in news_text:
                 sleep(randint(1, 3))
                 # send text of news
-                BOT.send_message("-1001501024008", i)
+                BOT.send_message("CHAT_ID", i)
                 sleep(randint(1, 3))
                 # send translated text of news
-                BOT.send_message('-1001501024008', TRANSLATOR.translate(i, src='cs', dest='ru').text)
+                BOT.send_message('CHAT_ID', TRANSLATOR.translate(i, src='cs', dest='ru').text)
 
             news = act_news
 
