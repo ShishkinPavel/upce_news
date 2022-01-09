002f1f9086156be74fd7a4f12b696ee0007fbb01
Pavel Shishkin <41304680+ShishkinPavel@users.noreply.github.com>
2022-01-09 12:52:05 +0100

    Update upce_updates.py

--- upce_updates.py
+++ upce_updates.py
@@ -3,7 +3,8 @@ import re
 from bs4 import BeautifulSoup
 import requests
 from random import randint
-from time import sleep
+import time
+import datetime
 from telebot import TeleBot
 from googletrans import Translator
 
@@ -24,40 +25,46 @@ def send_updates():
     # get links of news
     news = get_links()
     while True:
-        sleep(randint(15000, 17000))
-        # get links of news one more time
-        act_news = get_links()
-        # check if there are new links of news
-        if act_news != news:
-            # get text for every news
-            news_text = get_news([i for i in act_news if i not in news])
-            for i in news_text:
-                sleep(randint(1, 3))
-                # send text of news
-                BOT.send_message("CHAT_ID", i)
-                sleep(randint(1, 3))
-                # send translated text of news
-                BOT.send_message('CHAT_ID', TRANSLATOR.translate(i, src='cs', dest='ru').text)
-
+        # for repeating 1 per hour I need do this because sleep worked weird on Heroku
+        current_time = datetime.datetime.now()
+        if current_time.minute == 0:
+            # get links of news one more time
+            act_news = get_links()
+            # check if there are new links of news
+            if act_news != news:
+                # get text for every news
+                news_text = get_news([i for i in act_news if i not in news])
+                for i in news_text:
+                    time.sleep(randint(1, 3))
+                    # send text of news
+                    BOT.send_message("CHAT_ID", i)
+                    time.sleep(randint(1, 3))
+                    # send translated text of news
+                    BOT.send_message(
+                        "CHAT_ID",
+                        TRANSLATOR.translate(i, src="cs", dest="ru").text,
+                    )
             news = act_news
+            time.sleep(70)
 
 
 def get_links():
-    link = 'https://www.upce.cz/novinky'
-    links = get_soup(link). \
-        find_all("div", {"class": "col-xs-12 col-sm-6 col-md-4 xs-mb-4 sm-mb-0"})
+    link = "https://www.upce.cz/novinky"
+    links = get_soup(link).find_all(
+        "div", {"class": "col-xs-12 col-sm-6 col-md-4 xs-mb-4 sm-mb-0"}
+    )
     # reverse to chronological
     links.reverse()
-    links = [i.find('a')['href'] for i in links]
+    links = [i.find("a")["href"] for i in links]
     return links
 
 
 def get_news(links):
     result = []
     for link in links:
-        text = get_soup(link).find(id='block-system-main').text
+        text = get_soup(link).find(id="block-system-main").text
         # replace multiply \n to one \n
-        result.append(re.sub(r'\n+', '\n', text).strip())
+        result.append(re.sub(r"\n+", "\n", text).strip())
     return result
 
 
