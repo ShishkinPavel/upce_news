d23d44b46b82f2d40a8331e8be59e4e57e39f8bb
Pavel Shishkin <41304680+ShishkinPavel@users.noreply.github.com>
2022-01-10 09:42:20 +0100

    Update upce_updates.py

--- upce_updates.py
+++ upce_updates.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 import requests
 from random import randint
 import time
-import datetime
 from telebot import TeleBot
 from googletrans import Translator
 
+HEADERS = {"YOUR": "HEADERS"}
 
 # translator object
 TRANSLATOR = Translator()
@@ -17,7 +17,7 @@ BOT = TeleBot("BOT_API")
 
 # return soup object of webpage by link
 def get_soup(link):
-    return BeautifulSoup(requests.get(link).text, features="lxml")
+    return BeautifulSoup(requests.get(link, headers=HEADERS).text, "html.parser")
 
 
 # main function to send msgs
@@ -25,27 +25,26 @@ def send_updates():
     # get links of news
     news = get_links()
     while True:
-        # for repeating 1 per hour I need do this because sleep worked weird on Heroku
-        current_time = datetime.datetime.now()
-        if current_time.minute == 0:
-            # get links of news one more time
-            act_news = get_links()
-            # check if there are new links of news
-            if act_news != news:
-                # get text for every news
-                news_text = get_news([i for i in act_news if i not in news])
-                for i in news_text:
-                    time.sleep(randint(1, 3))
-                    # send text of news
-                    BOT.send_message("CHAT_ID", i)
-                    time.sleep(randint(1, 3))
-                    # send translated text of news
-                    BOT.send_message(
-                        "CHAT_ID",
-                        TRANSLATOR.translate(i, src="cs", dest="ru").text,
-                    )
+        # for scraping once per hour
+        time.sleep(3600)
+        # get links of news one more time
+        act_news = get_links()
+        # check if there are new links of news
+        if act_news != news:
+            # get text for every news
+            news_text = get_news([i for i in act_news if i not in news])
+            for i in news_text:
+                time.sleep(randint(1, 3))
+                # send text of news
+                BOT.send_message("CHAT_ID", i)
+                time.sleep(randint(1, 3))
+                # send translated text of news
+                BOT.send_message(
+                    "CHAT_ID",
+                    TRANSLATOR.translate(i, src="cs", dest="ru").text,
+                )
+
             news = act_news
-            time.sleep(70)
 
 
 def get_links():
@@ -64,8 +63,9 @@ def get_news(links):
     for link in links:
         text = get_soup(link).find(id="block-system-main").text
         # replace multiply \n to one \n
-        result.append(re.sub(r"\n+", "\n", text).strip())
+        result.append(re.sub(r"\n+", "\n", text).strip() + 3 * "\n" + link)
     return result
 
 
-send_updates()
+if __name__ == '__main__':
+    send_updates()
